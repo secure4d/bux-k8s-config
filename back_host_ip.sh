f6b0c0ddb499633c847fd60c7c8b1001d78d6d7b
secure4d <handson@secure4d.co.jp>
2024-03-07 00:35:11 +0900

    Refresh

--- back_host_ip.sh
+++
@@ -1,2 +0,0 @@
-sed -i "s/172.233.79.213/BUX_HOST_IP/g" install_microk8s.sh
-sed -i "s/172.233.79.213/BUX_HOST_IP/g" microk8s-config.yaml
--- install_microk8s.sh
+++ install_microk8s.sh
@@ -12,7 +12,8 @@ sudo microk8s enable dns
 sudo microk8s enable dashboard
 sudo microk8s enable helm
 sudo microk8s enable helm3
-sudo microk8s enable argocd:v2.3.15
+sudo microk8s enable argocd
 sudo microk8s enable cert-manager
 sudo microk8s enable openebs
 sudo microk8s enable metallb BUX_HOST_IP/32
+sudo microk8s enable traefik --set="additionalArguments={--serverstransport.insecureskipverify=true,--providers.kubernetesingress.ingressendpoint.publishedservice=traefik/traefik}"
